0289c64824dce30c061cfcefdbee7b48cb060c3f
RajasreeFT <rajasreefuturetech@gmail.com>
2025-10-16 16:32:24 +0530

    latest

--- marketingbackend/settings.py
+++ marketingbackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-zf&$=#uuonrlj-bd@17c(6if#$txhcu)%im8_usw1$%6!feo5n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
